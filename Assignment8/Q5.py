586c659263c3b48adf721bb5262c2de5553b2b08
Butpizz <narensk6@gmail>
2022-09-19 13:18:45 +0530

    first commit

---
+++ Assignment8/Q5.py
@@ -0,0 +1,11 @@
+# 5. Write a python script to print first 10 odd natural numbers in reverse order
+
+# i = 19
+# while i >= 1:
+#     print(i, end=' ')
+#     i -= 2
+
+i = 10
+while i > 0:
+    print(i*2-1, end=' ')
+    i -= 1
---
+++ Assignment8/Q7.py
@@ -0,0 +1,12 @@
+# 7. Write a python script to print first 10 even natural numbers in reverse order
+
+# i = 20
+# while i >= 2:
+#     print(i, end=' ')
+#     i -= 2
+
+i = 10
+
+while i > 0:
+    print(i*2, end=' ')
+    i -= 1
