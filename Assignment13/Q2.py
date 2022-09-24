79886f31d4e2e5027730dc2ea75e9aa504be0deb
Butpizz <narensk6@gmail>
2022-09-24 11:50:34 +0530

    first commit

---
+++ Assignment13/Q2.py
@@ -0,0 +1,4 @@
+# 2. Write a python script to get the data type of a list.
+
+list = ['Java', 'Python', 'SQL', 'C']
+print(type(list))
---
+++ Assignment14/Q10.py
@@ -0,0 +1,7 @@
+# 10. Write a python script to sort a list.
+li = [60, 20, 10, 20, 30, 30, 30, 40]
+
+print(li, end='\n\n')
+li.sort()
+
+print(li)
